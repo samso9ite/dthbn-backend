4a75232e7bef02b31aa549d4516ec5add8850ce6
Samso9ite <samso9ite@gmail.com>
2024-03-18 14:01:08 +0100

    Prof Dashboard Added

--- profPortal/urls.py
+++ profPortal/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     # path('update_account/<int:pk>', views.ProfAccntUpdateView.as_view(), name='accountUpdate'),
 
     path('add-profile', views.AddProfessionalView.as_view(), name="add_profile"),
-    path('update-profile', views.UpdateProfileView.as_view(), name="update_profile"),
+    path('update-profile/<int:id>', views.UpdateProfileView.as_view(), name="update_profile"),
     path('get-profile/<int:profuser>', views.RetrieveProfView.as_view(), name='get-profile'),
-    path('list-license/<int:id>', views.ListLicenseView.as_view(), name="list_license")
+    path('list-license/<int:id>', views.ListLicenseView.as_view(), name="list_license"),
+    path('dashboard', views.profDashboard, name="prof_dashboard"),
 ] 
--- profPortal/views.py
+++ profPortal/views.py
@@ -8,6 +8,11 @@ from django.urls import reverse_lazy
 from rest_framework.generics import UpdateAPIView, RetrieveAPIView, CreateAPIView, ListAPIView
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from authentication.serializers import userSerializer
 
 # Create your views here
 class UpdateProfileView(UpdateAPIView):
@@ -31,3 +36,25 @@ class ListLicenseView(ListAPIView):
     serializer_class = licenseSerializer
     permission_classes = [IsAuthenticated]
     lookup_field = 'id'
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profDashboard(request):
+    user=request.user
+    print(user)
+    if user.is_professional:
+        print(user)
+        if user.is_active:
+            print("I'm here now")
+            license = licenseModel.objects.filter(prof_id=user.id)
+            serialized_license = licenseSerializer(license, many=True).data
+            serialized_user = userSerializer(user).data
+            license_count = license.count()
+            print(license_count)
+            context = {
+                'license':serialized_license,
+                'license_count': license_count,
+                'user':serialized_user
+            }
+            return Response({"data": context, "message": "Request Successfull"}, status=status.HTTP_200_OK)
+    return Response({"message": "User not Professional"}, status=status.HTTP_404_NOT_FOUND)
